4656edfa75fe559b370eacfe9da98cf6e28d76f0
stuty.mishra123 <stuty.mishra123@gmail.com>
2021-07-29 14:10:43 +0530

    commented code removed

--- src/app/module/weather/home-page/home-page.component.ts
+++ src/app/module/weather/home-page/home-page.component.ts
@@ -11,11 +11,17 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 export class HomePageComponent implements OnInit {
 
   cityData;
+  title;
   constructor(private http: HttpClient,
               private route: ActivatedRoute,
               private router: Router) { }
-    title;
+   
   ngOnInit() {
+
+    /* Found Ids of 5 cities in London circle using API " http://api.openweathermap.org/data/2.5/find?lat=51.5085&lon=-0.1257&cnt=5&appid=3d8b309701a13f65b660fa2c64cdc517" used those IDs to fetch data for each city .
+
+    Used individual API as the API that takes multiple cities does not have "sys" object to fetch sunset and sunrise time  */
+
     let serviceData1 = this.http.get("https://openweathermap.org/data/2.5/weather?id=2643743&appid=439d4b804bc8187953eb36d2a8c26a02");
     let serviceData2 = this.http.get("https://openweathermap.org/data/2.5/weather?id=2634341&appid=439d4b804bc8187953eb36d2a8c26a02");
     let serviceData3 = this.http.get("https://openweathermap.org/data/2.5/weather?id=6545250&appid=439d4b804bc8187953eb36d2a8c26a02");
@@ -27,14 +33,13 @@ export class HomePageComponent implements OnInit {
     let multicall = forkJoin(arrayCall);
     multicall.subscribe(
       data => {
-        console.log(data);
         this.cityData = data;
       },
       error => {
         console.log(error);
       }
     );
-    this.title = `Current Weather details for 5 cities`
+    this.title = `Current Weather details for 5 cities`;
   }
 
   navigateTodetails = (id) => {
